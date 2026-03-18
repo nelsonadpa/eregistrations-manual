269e2f9650bd3686432343cb0f21f76a9604c381
Nelson PEREZ <nelsonperez@MacBook-Air.local>
2026-03-18 22:16:53 +0100

    feat: add Lesotho country config

---
+++ lesotho/assets/config.js
@@ -0,0 +1,17 @@
+window.MANUAL_CONFIG = {
+    country: "Lesotho",
+    orgName: "One Business Facilitation Centre (OBFC)",
+    orgShort: "OBFC",
+    platformUrl: "https://services.businessregistrations.gov.ls/",
+    aboutUrl: "https://lesotho.eregistrations.dev/about/",
+    helpUrl: "https://lesotho.eregistrations.dev/help/",
+    homeUrl: "https://lesotho.eregistrations.dev/",
+    primaryColor: "#4CAF74",
+    headerBg: "#2B2A65",
+    logoUrl: "https://lesotho.eregistrations.dev/_assets/v11/83b89d968e865c9d23a548390d2f71bd27ec2ecd.png",
+    coatOfArmsUrl: "https://lesotho.eregistrations.dev/_assets/v11/62e7e38d29393d9e912a3aa2888bec679106a457.png",
+    processingPassword: "lesotho",
+    contactEmail: "info@obfc.gov.ls",
+    contactPhone: "+266 2232 7594",
+    lastUpdated: "2026-03-18"
+};
